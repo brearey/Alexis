f80f27fb09b6eafed5cc06fe806d759b3e033d47
Brearey <brearey4@gmail.com>
2022-12-07 14:39:00 +0900

    # save answers to mutableListOf int

--- app/src/main/java/ru/oktemsec/sashapopov/QuizActivity.kt
+++ app/src/main/java/ru/oktemsec/sashapopov/QuizActivity.kt
@@ -2,46 +2,68 @@ package ru.oktemsec.sashapopov
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.TextView
 
 class QuizActivity : AppCompatActivity() {
+
+    private val TAG = QuizActivity::class.java.simpleName
+    private val arrayOfAnswers = mutableListOf<Int>()
+
+    private lateinit var questionTextView:TextView
+    private lateinit var positiveButton:Button
+    private lateinit var negativeButton:Button
+    private lateinit var difficultButton:Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_quiz)
 
-        val questionTextView: TextView = findViewById(R.id.question_tv)
-        val positiveButton: Button = findViewById(R.id.positive_button)
-        val negativeButton: Button = findViewById(R.id.negative_button)
-        val difficultButton: Button = findViewById(R.id.difficult_button)
+        questionTextView = findViewById(R.id.question_tv)
+        positiveButton = findViewById(R.id.positive_button)
+        negativeButton = findViewById(R.id.negative_button)
+        difficultButton = findViewById(R.id.difficult_button)
 
         var indexOfQuestion = 0
 
         positiveButton.setOnClickListener {
             indexOfQuestion++
             questionTextView.text = nextQuestion(indexOfQuestion)
+            arrayOfAnswers.add(0)
         }
         negativeButton.setOnClickListener {
             indexOfQuestion++
             questionTextView.text = nextQuestion(indexOfQuestion)
+            arrayOfAnswers.add(1)
         }
         difficultButton.setOnClickListener {
             indexOfQuestion++
             questionTextView.text = nextQuestion(indexOfQuestion)
+            arrayOfAnswers.add(2)
         }
     }
 
     private fun nextQuestion(index:Int): String {
         val questionRepository = QuestionRepository()
         return if (index < questionRepository.questions.size) questionRepository.questions[index]
-        else "no questions"
+        else {
+            positiveButton.isEnabled = false
+            negativeButton.isEnabled = false
+            difficultButton.isEnabled = false
+            Log.d(TAG, arrayOfAnswers.toString())
+            return "no questions"
+        }
     }
 }
 
-class QuestionRepository() {
-    val questions = arrayOf<String>(
+class QuestionRepository {
+    val questions = arrayOf (
         "1 Головные боли, головокружения, болевые ощущения в ногах при ходьбе, боли в груди?",
         "2 Головные боли, головокружения, болевые ощущения в ногах при ходьбе, боли в груди?",
         "3 Головные боли, головокружения, болевые ощущения в ногах при ходьбе, боли в груди?",
+        "4 Головные боли, головокружения, болевые ощущения в ногах при ходьбе, боли в груди?",
+        "5 Головные боли, головокружения, болевые ощущения в ногах при ходьбе, боли в груди?",
+        "6 Головные боли, головокружения, болевые ощущения в ногах при ходьбе, боли в груди?",
     )
 }
