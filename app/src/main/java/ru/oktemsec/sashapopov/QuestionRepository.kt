f1c764f3a72534e56191ac3a0ee952105b9765d0
Brearey <brearey4@gmail.com>
2023-01-05 12:37:35 +0900

    # вопросы делятся на с температурой и без # результат по количеству агентов той или иной болезни

--- app/src/main/java/ru/oktemsec/sashapopov/QuestionRepository.kt
+++ app/src/main/java/ru/oktemsec/sashapopov/QuestionRepository.kt
@@ -2,9 +2,182 @@ package ru.oktemsec.sashapopov
 
 class QuestionRepository {
     val questions = arrayOf (
-        "Головные боли, головокружения, болевые ощущения в ногах при ходьбе, боли в груди?",
-        "Боль в позвоночнике?",
-        "Бледность лица, посинение губ, мочек ушей, ног?",
-        "Повышенная утомляемость, слабость, дрожь в руках?",
+        Question(
+            questionText = "Чувствуете ли вы болезненные ощущения при движении глаз?",
+            painORVI = 1,
+            painMeningit = 1,
+            painORZ = 1,
+            painNevralgia = 0,
+            highTemp = true
+        ),
+        Question(
+            questionText = "Отсутствие аппетита?",
+            painORVI = 1,
+            painMeningit = 1,
+            painORZ = 1,
+            painNevralgia = 0,
+            highTemp = true
+        ),
+        Question(
+            questionText = "Боли в мышцах, в грудной клетке?",
+            painORVI = 1,
+            painMeningit = 1,
+            painORZ = 0,
+            painNevralgia = 0,
+            highTemp = true
+        ),
+        Question(
+            questionText = "Сильные боли в горле?",
+            painORVI = 1,
+            painMeningit = 1,
+            painORZ = 0,
+            painNevralgia = 0,
+            highTemp = true
+        ),
+        Question(
+            questionText = "Заложенность носа?",
+            painORVI = 1,
+            painMeningit = 0,
+            painORZ = 0,
+            painNevralgia = 0,
+            highTemp = true
+        ),
+        // вторая линия
+        Question(
+            questionText = "Упадок сил?",
+            painORVI = 0,
+            painMeningit = 1,
+            painORZ = 1,
+            painNevralgia = 0,
+            highTemp = true
+        ),
+        Question(
+            questionText = "Тошнота?",
+            painORVI = 0,
+            painMeningit = 1,
+            painORZ = 0,
+            painNevralgia = 0,
+            highTemp = true
+        ),
+        Question(
+            questionText = "Проблемы со слухом?",
+            painORVI = 0,
+            painMeningit = 1,
+            painORZ = 0,
+            painNevralgia = 0,
+            highTemp = true
+        ),
+        Question(
+            questionText = "Ригидность мышц затылка?",
+            painORVI = 0,
+            painMeningit = 1,
+            painORZ = 0,
+            painNevralgia = 0,
+            highTemp = true
+        ),
+        Question(
+            questionText = "Сонливость?",
+            painORVI = 0,
+            painMeningit = 1,
+            painORZ = 1,
+            painNevralgia = 0,
+            highTemp = true
+        ),
+        Question(
+            questionText = "Метеоризм?",
+            painORVI = 0,
+            painMeningit = 1,
+            painORZ = 1,
+            painNevralgia = 0,
+            highTemp = true
+        ),
+        Question(
+            questionText = "Першение и боль в горле?",
+            painORVI = 0,
+            painMeningit = 1,
+            painORZ = 1,
+            painNevralgia = 0,
+            highTemp = true
+        ),
+        Question(
+            questionText = "Сухой или влажный кащель?",
+            painORVI = 0,
+            painMeningit = 1,
+            painORZ = 1,
+            painNevralgia = 0,
+            highTemp = true
+        ),
+        Question(
+            questionText = "Боль в височной или лобной области?",
+            painORVI = 0,
+            painMeningit = 1,
+            painORZ = 0,
+            painNevralgia = 1,
+            highTemp = false
+        ),
+        Question(
+            questionText = "Пульсирующая боль?",
+            painORVI = 0,
+            painMeningit = 1,
+            painORZ = 0,
+            painNevralgia = 1,
+            highTemp = false
+        ),
+        Question(
+            questionText = "Боль увеличивается при физ. нагрузке?",
+            painORVI = 0,
+            painMeningit = 1,
+            painORZ = 0,
+            painNevralgia = 1,
+            highTemp = false
+        ),
+        Question(
+            questionText = "Повышенная чувстыительность к свету?",
+            painORVI = 0,
+            painMeningit = 1,
+            painORZ = 0,
+            painNevralgia = 0,
+            highTemp = false
+        ),
+        Question(
+            questionText = "Тошнота?",
+            painORVI = 0,
+            painMeningit = 1,
+            painORZ = 0,
+            painNevralgia = 0,
+            highTemp = false
+        ),
+        Question(
+            questionText = "Светобоязнь?",
+            painORVI = 0,
+            painMeningit = 1,
+            painORZ = 0,
+            painNevralgia = 0,
+            highTemp = false
+        ),
+        Question(
+            questionText = "Головокружение?",
+            painORVI = 0,
+            painMeningit = 0,
+            painORZ = 0,
+            painNevralgia = 1,
+            highTemp = false
+        ),
+        Question(
+            questionText = "Беспричинное слезотечение?",
+            painORVI = 0,
+            painMeningit = 0,
+            painORZ = 0,
+            painNevralgia = 1,
+            highTemp = false
+        ),
+        Question(
+            questionText = "Боль напоминающий удар током?",
+            painORVI = 0,
+            painMeningit = 0,
+            painORZ = 0,
+            painNevralgia = 1,
+            highTemp = false
+        ),
     )
 }
--- app/src/main/java/ru/oktemsec/sashapopov/QuizActivity.kt
+++ app/src/main/java/ru/oktemsec/sashapopov/QuizActivity.kt
@@ -5,11 +5,23 @@ import android.os.Bundle
 import android.util.Log
 import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
 
 class QuizActivity : AppCompatActivity() {
 
     private val TAG = QuizActivity::class.java.simpleName
-    private val arrayOfAnswers = mutableListOf<Int>()
+
+    // Agents
+    private var agentsOfPainORVI: Int = 0
+    private var agentsOfPainMeningit: Int = 0
+    private var agentsOfPainORZ: Int = 0
+    private var agentsOfPainNevralgia: Int = 0
+
+    // High temp
+    private var highTemp = false
+
+    // object of Question Repo
+    private val questionRepository = QuestionRepository()
 
     private lateinit var questionTextView:TextView
     private lateinit var positiveButton:Button
@@ -28,35 +40,64 @@ class QuizActivity : AppCompatActivity() {
         var indexOfQuestion = 0
 
         positiveButton.setOnClickListener {
+            if (indexOfQuestion == 0) {
+                highTemp = true
+                questionTextView.text = nextQuestion(indexOfQuestion).questionText
+            } else {
+                val oneQuestion = nextQuestion(indexOfQuestion)
+                agentsOfPainORVI += oneQuestion.painORVI
+                agentsOfPainMeningit += oneQuestion.painMeningit
+                agentsOfPainORZ += oneQuestion.painORZ
+                agentsOfPainNevralgia += oneQuestion.painNevralgia
+
+                questionTextView.text = oneQuestion.questionText
+
+                // For debug
+                Log.d("brearey", "Агенты ОРВИ: $agentsOfPainORVI")
+                Log.d("brearey", "Агенты Менингита: $agentsOfPainMeningit")
+                Log.d("brearey", "Агенты ОРЗ: $agentsOfPainORZ")
+                Log.d("brearey", "Агенты Невралгии: $agentsOfPainNevralgia")
+            }
             indexOfQuestion++
-            arrayOfAnswers.add(0)
-            questionTextView.text = nextQuestion(indexOfQuestion)
         }
         negativeButton.setOnClickListener {
+            if (indexOfQuestion == 0) {
+                highTemp = false
+                questionTextView.text = nextQuestion(indexOfQuestion).questionText
+            } else {
+                questionTextView.text = nextQuestion(indexOfQuestion).questionText
+            }
             indexOfQuestion++
-            arrayOfAnswers.add(1)
-            questionTextView.text = nextQuestion(indexOfQuestion)
         }
         difficultButton.setOnClickListener {
             indexOfQuestion++
-            arrayOfAnswers.add(2)
-            questionTextView.text = nextQuestion(indexOfQuestion)
+            questionTextView.text = nextQuestion(indexOfQuestion).questionText
         }
     }
 
-    private fun nextQuestion(index:Int): String {
-
-        Log.d("brearey", arrayOfAnswers.toString())
-
-        val questionRepository = QuestionRepository()
-        return if (index < questionRepository.questions.size) questionRepository.questions[index]
+    private fun nextQuestion(index:Int): Question {
+        val questions = emptyList<Question>().toMutableList()
+        questionRepository.questions.forEach {
+            if (it.highTemp == highTemp) questions += it
+        }
+        return if (index < questions.size) questions[index]
         else {
             positiveButton.isEnabled = false
             negativeButton.isEnabled = false
             difficultButton.isEnabled = false
-            return "no questions"
 
             // TODO: startActivity
+            val painsList = listOf("ОРВИ", "Менингит", "ОРЗ", "Невралгия")
+            val agentsList = listOf(
+                agentsOfPainORVI,
+                agentsOfPainMeningit,
+                agentsOfPainORZ,
+                agentsOfPainNevralgia
+            )
+            val maxIdx = agentsList.indices.maxBy { agentsList[it] }
+            Toast.makeText(this, "Ваш результат:\nБольшая вероятность что у вас: ${painsList[maxIdx]}", Toast.LENGTH_LONG).show()
+
+            return Question(questionText = "Вопросы закончились", painORVI = 0, painMeningit = 0, painORZ = 0, painNevralgia = 0, highTemp = false)
         }
     }
 }
