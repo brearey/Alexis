ecaf93a2dc3688af31ad0befb17b382602375203
brearey <brearey4@gmail.com>
2023-03-21 16:25:02 +0900

    Activity meningit begin

---
+++ app/src/main/java/ru/oktemsec/sashapopov/MeningitActivity.kt
@@ -0,0 +1,11 @@
+package ru.oktemsec.sashapopov
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MeningitActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_meningit)
+    }
+}
