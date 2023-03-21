8e107ad66bd49b6acb36348687ea09e9b2c61cee
brearey <brearey4@gmail.com>
2023-03-21 16:48:01 +0900

    Function call added

--- app/src/main/java/ru/oktemsec/sashapopov/MainActivity.kt
+++ app/src/main/java/ru/oktemsec/sashapopov/MainActivity.kt
@@ -1,9 +1,9 @@
 package ru.oktemsec.sashapopov
 
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.widget.ImageButton
 
 class MainActivity : AppCompatActivity() {
@@ -12,10 +12,21 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val healthButton = findViewById<ImageButton>(R.id.health_button)
+        val sosButton = findViewById<ImageButton>(R.id.sos_button)
 
         healthButton.setOnClickListener {
             startActivity(Intent(this@MainActivity, PainZoneActivity::class.java))
         }
 
+        sosButton.setOnClickListener {
+            call()
+        }
+
+    }
+
+    private fun call() {
+        val dialIntent = Intent(Intent.ACTION_DIAL)
+        dialIntent.data = Uri.parse("tel:" + "103")
+        startActivity(dialIntent)
     }
 }
